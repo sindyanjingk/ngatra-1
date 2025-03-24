9309be93a13ca70f2a109a2ba567cadf6c1af748
JalalDA <jalaldalkhotami@gmail.com>
2025-03-24 19:45:16 +0700

    feat:add page

--- app/[domain]/(dashboard)/orders/page.tsx
+++ app/[domain]/(dashboard)/orders/page.tsx
@@ -1,8 +1,27 @@
+import prisma from '@/lib/prisma';
 import React from 'react'
 
 type Props = {}
 
-const Orders = (props: Props) => {
+const Orders = async ({
+  params,
+}: {
+  params: { domain: string; slug: string };
+}) => {
+  const domain = params.domain.split('.')[0];
+  const site = await prisma.sites.findFirst({
+    where: {
+      subdomain: domain
+    }
+  })
+
+  const transaction = await prisma.transaction.findMany({
+    where : {
+      siteId : site?.id
+    }
+  })
+  console.log({transaction});
+  
   return (
     <div>Orders</div>
   )
